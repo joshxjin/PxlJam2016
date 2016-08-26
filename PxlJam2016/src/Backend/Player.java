160e89b76dd240da2fc185d545f7e94fc61b90e6
joshxjin <joshxjin@gmail.com>
2016-08-26 22:00:38 +1200

    changed player x and y to static and added static getXX and getYY methods

--- PxlJam2016/src/Backend/Player.java
+++ PxlJam2016/src/Backend/Player.java
@@ -4,11 +4,12 @@ import processing.core.PApplet;
 import processing.core.PConstants;
 
 public class Player extends GameObject {
-	
+
 	PApplet parent;
 	float DX, DY;
-	
-	public Player(PApplet p, float xx, float yy, float speedx, float speedy){
+	static float x, y;
+
+	public Player(PApplet p, float xx, float yy, float speedx, float speedy) {
 		parent = p;
 		x = xx;
 		y = yy;
@@ -16,78 +17,77 @@ public class Player extends GameObject {
 		DY = speedy;
 		size = 30;
 	}
-	
-	public void setDir(){
-		if (parent.key == PConstants.CODED) { 
-		    if (parent.keyCode == PConstants.UP) {
-		    	dy = -DY;
-		    } 
-		    else if (parent.keyCode == PConstants.DOWN) {
-		    	dy = DY;
-		    }
-		    else if (parent.keyCode == PConstants.LEFT){
-		    	dx = -DX;
-		    }
-		    else if (parent.keyCode == PConstants.RIGHT){
-		    	dx = DX;
-		    }
-		  }
-		
-		else {
-			if(parent.key == 'w'){
-		    	dy = -DY;				
+
+	public void setDir() {
+		if (parent.key == PConstants.CODED) {
+			if (x - size <= 0 || x + size >= parent.width) {
+				dx = 0;
+			} else if (y - size <= 0 || y + size >= parent.height) {
+				dy = 0;
+			} else if (parent.keyCode == PConstants.UP) {
+				dy = -DY;
+			} else if (parent.keyCode == PConstants.DOWN) {
+				dy = DY;
+			} else if (parent.keyCode == PConstants.LEFT) {
+				dx = -DX;
+			} else if (parent.keyCode == PConstants.RIGHT) {
+				dx = DX;
 			}
-		    else if (parent.key == 's') {
-		    	dy = DY;
-		    }
-		    else if (parent.key == 'a'){
-		    	dx = -DX;
-		    }
-		    else if (parent.key == 'd'){
-		    	dx = DX;
-		    }
+		}
+
+		else {
+			if (parent.key == 'w') {
+				dy = -DY;
+			} else if (parent.key == 's') {
+				dy = DY;
+			} else if (parent.key == 'a') {
+				dx = -DX;
+			} else if (parent.key == 'd') {
+				dx = DX;
+			}
 		}
 	}
-	
-	public void move(){
+
+	public void move() {
 		x = x + dx;
 		y = y + dy;
 	}
-	
-	public void stopDir(){
-		if (parent.key == PConstants.CODED) { 
-		    if (parent.keyCode == PConstants.UP) {
-		    	dy = 0;
-		    } 
-		    else if (parent.keyCode == PConstants.DOWN) {
-		    	dy = 0;
-		    }
-		    else if (parent.keyCode == PConstants.LEFT){
-		    	dx = 0;
-		    }
-		    else if (parent.keyCode == PConstants.RIGHT){
-		    	dx = 0;
-		    }
-		  }
-		else{
-		    if (parent.key == 'w') {
-		    	dy = 0;
-		    } 
-		    else if (parent.key == 's') {
-		    	dy = 0;
-		    }
-		    else if (parent.key == 'a'){
-		    	dx = 0;
-		    }
-		    else if (parent.key == 'd'){
-		    	dx = 0;
-		    }
+
+	public void stopDir() {
+		if (parent.key == PConstants.CODED) {
+			if (parent.keyCode == PConstants.UP) {
+				dy = 0;
+			} else if (parent.keyCode == PConstants.DOWN) {
+				dy = 0;
+			} else if (parent.keyCode == PConstants.LEFT) {
+				dx = 0;
+			} else if (parent.keyCode == PConstants.RIGHT) {
+				dx = 0;
+			}
+		} else {
+			if (parent.key == 'w') {
+				dy = 0;
+			} else if (parent.key == 's') {
+				dy = 0;
+			} else if (parent.key == 'a') {
+				dx = 0;
+			} else if (parent.key == 'd') {
+				dx = 0;
+			}
 		}
 	}
-	
-	public void draw(){
+
+	public void draw() {
 		parent.fill(0);
-		parent.ellipse(x,y,size,size);
+		parent.ellipse(x, y, size, size);
+	}
+	
+	public static float getXX() {
+		return x;
+	}
+	
+	public static float getYY() {
+		return y;
 	}
 	
 }
