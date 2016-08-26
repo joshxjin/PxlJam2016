af20a09346aa4c6b77eda2b80f46c07f284b38e5
joshxjin <joshxjin@gmail.com>
2016-08-26 21:14:14 +1200

    added monster and obstacle class from Michael.

--- PxlJam2016/src/Backend/Obstacle.java
+++ PxlJam2016/src/Backend/Obstacle.java
@@ -1,5 +1,41 @@
 package Backend;
 
-public class Obstacle {
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+
+public class Obstacle extends GameObject {
+
+	private PApplet parent;
+	private static ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
+
+	public Obstacle(PApplet p, float x, float y) {
+		this.parent = p;
+		this.x = x;
+		this.y = y;
+		this.dx = 0;
+		this.dy = 0;
+		this.size = 30;
+		obstacles.add(this);
+	}
+
+	public float getX() {
+		return this.x;
+	}
+
+	public float getY() {
+		return this.y;
+	}
+
+	public static ArrayList<Obstacle> getObstacles() {
+		return obstacles;
+	}
+
+	public static void showObstacles() {
+		for (Obstacle o : obstacles) {
+			o.parent.fill(0, 0, 255);
+			o.parent.rect(o.x, o.y, o.size, o.size);
+		}
+	}
 
 }
